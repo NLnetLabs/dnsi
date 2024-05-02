c5c83e450e01214b3fe3d53df2d0e6c5527fca1f
Martin Hoffmann <martin@nlnetlabs.nl>
2024-05-02 11:11:05 +0200

    Add a idns man command.

---
+++ src/idns/commands/help.rs
@@ -0,0 +1,55 @@
+//! The help command of _idns._
+
+use std::io::Write;
+use std::process::Command;
+use tempfile::NamedTempFile;
+use crate::idns::error::Error;
+
+
+//------------ Help ----------------------------------------------------------
+
+#[derive(Clone, Debug, clap::Args)]
+pub struct Help {
+    /// The command to show the man page for
+    #[arg(value_name="COMMAND")]
+    command: Option<String>,
+}
+
+impl Help {
+    pub fn execute(self) -> Result<(), Error> {
+        let page = match self.command.as_ref().map(String::as_str) {
+            None => Self::IDNS_1,
+            Some("query") => Self::IDNS_QUERY_1,
+            Some(command) => {
+                return Err(format!("Unknown command '{}'.", command).into());
+            }
+        };
+
+        let mut file = NamedTempFile::new().map_err(|err| {
+            format!(
+                "Can't display man page: \
+                 Failed to create temporary file: {}.",
+                err
+            )
+        })?;
+        file.write_all(page).map_err(|err| {
+            format!(
+                "Can't display man page: \
+                Failed to write to temporary file: {}.",
+                err
+            )
+        })?;
+        let _ = Command::new("man").arg(file.path()).status().map_err(|err| {
+            format!("Failed to run man: {}", err)
+        })?;
+        Ok(())
+    }
+}
+
+impl Help {
+    const IDNS_1: &'static [u8] = include_bytes!("../../../doc/idns.1");
+    const IDNS_QUERY_1: &'static [u8] = include_bytes!(
+        "../../../doc/idns-query.1"
+    );
+}
+
--- src/idns/commands/mod.rs
+++ src/idns/commands/mod.rs
@@ -1,5 +1,6 @@
 //! The various commands of _idsn._
 
+pub mod help;
 pub mod query;
 
 
@@ -10,12 +11,16 @@ use super::error::Error;
 pub enum Commands {
     /// Query the DNS.
     Query(self::query::Query),
+
+    /// Show the manual pages.
+    Man(self::help::Help),
 }
 
 impl Commands {
     pub fn execute(self) -> Result<(), Error> {
         match self {
             Self::Query(query) => query.execute(),
+            Self::Man(help) => help.execute(),
         }
     }
 }
