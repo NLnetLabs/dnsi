15e23f4efa60b58a74414cd700c169a551eab805
Philip Homburg <philip@nlnetlabs.nl>
2024-10-18 15:38:30 +0200

    Wip

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 //! The _dnsi_ binary.
 
 use clap::Parser;
+use tracing_subscriber::EnvFilter;
 
 fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_thread_ids(true)
+        .without_time()
+        .try_init()
+        .ok();
+
     if let Err(err) = dnsi::Args::parse().execute() {
         eprintln!("{}", err);
     }
